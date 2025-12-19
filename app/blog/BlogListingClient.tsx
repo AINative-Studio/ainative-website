c46594e05bbb21e7c142ab17a73bf71896d879e8
Karsten Wade <quaid@iquaid.org>
2025-12-19 14:16:38 -0800

    feat: Add Eye icon to blog view count display
    - Import Eye icon from lucide-react
    - Add Eye icon to view count display on blog listing cards
    - Matches upstream styling for view count display
    
    Closes #178

--- app/blog/BlogListingClient.tsx
+++ app/blog/BlogListingClient.tsx
@@ -14,6 +14,7 @@ import { searchCommunityContent } from '@/src/lib/community/search';
 import {
   Calendar,
   Clock,
+  Eye,
   Search,
   AlertCircle,
   Zap
@@ -427,7 +428,10 @@ export default function BlogListingClient() {
                           <Calendar className="h-4 w-4 mr-1" />
                           {new Date(post.published_date).toLocaleDateString('en-US', { month: 'short', day: 'numeric' })}
                         </div>
-                        <span>{post.view_count.toLocaleString()} views</span>
+                        <div className="flex items-center">
+                          <Eye className="h-4 w-4 mr-1" />
+                          <span>{post.view_count.toLocaleString()} views</span>
+                        </div>
                       </div>
                       {post.tags && post.tags.length > 0 && (
                         <div className="flex flex-wrap gap-1 mt-3">
